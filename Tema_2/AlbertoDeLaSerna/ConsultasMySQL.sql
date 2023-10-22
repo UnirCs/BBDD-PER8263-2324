fb68aedf150481a2b8806e17e7b9389df172df97
Alberto de la Serna <alberto.delaserna738@comunidadunir.net>
2023-10-22 16:09:28 +0200

    Primeras queries

---
+++ Tema_2/AlbertoDeLaSerna/ConsultasMySQL.sql
@@ -0,0 +1,27 @@
+# Número de hombre y mujeres
+select count(*) as total, gender from employees group by gender order by total desc;
+
+# Mostrar el nombre, apellido y salario de la persona mejor pagada de un departamento concreto
+select first_name, last_name, salary from employees
+    inner join dept_emp on employees.emp_no = dept_emp.emp_no
+    inner join departments on dept_emp.dept_no = departments.dept_no and dept_emp.emp_no = employees.emp_no
+    inner join salaries on employees.emp_no = salaries.emp_no
+where dept_name = 'Development'
+order by salary desc
+limit 1;
+
+# Mostrar el nombre, apellido y salario de la segunda mejor pagada de un departamento concreo
+select first_name, last_name, salary from employees
+    inner join dept_emp on employees.emp_no = dept_emp.emp_no
+    inner join departments on dept_emp.dept_no = departments.dept_no and dept_emp.emp_no = employees.emp_no
+    inner join salaries on employees.emp_no = salaries.emp_no
+where dept_name = 'Development'
+order by salary desc
+limit 1,1;
+# El limit 1,1 => salta el primer registro con el primer uno y con el segundo muestra el segundo registro.
+
+
+select * from salaries;
+
+
+select * from departments
---
+++ Tema_2/AlbertoDeLaSerna/Readme.md
@@ -0,0 +1,15 @@
+# Alberto de la Serna - Tema 2 - Ejercicios
+
+## Dificultades encontradas
+
+Al bajarme el programa de Java para probar diferentes queries me he dado cuento que los conectores trabajan con variables de entorno.
+
+Para establecer las variables de entorno en el IDE, hay que ir al menú run y rellenar el apartado "Enviroment Variables": estas son las que he puesto: 
+
+Para OracleApplication:
+`ORACLE_USER=HR;ORACLE_PASSWORD=oracle`
+
+Para MySqlApplication:
+
+
+`MYSQL_USER=root;MYSQL_PASSWORD=mysql`
