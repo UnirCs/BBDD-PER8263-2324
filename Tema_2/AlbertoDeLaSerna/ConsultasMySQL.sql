da98da4537512c6129ed46b553bb9963aa29fdb5
Alberto de la Serna <alberto.delaserna738@comunidadunir.net>
2023-10-28 10:59:47 +0200

    oracle queries iniciales

--- Tema_2/AlbertoDeLaSerna/ConsultasMySQL.sql
+++ Tema_2/AlbertoDeLaSerna/ConsultasMySQL.sql
@@ -1,7 +1,7 @@
-# Número de hombre y mujeres
+-- Número de hombre y mujeres
 select count(*) as total, gender from employees group by gender order by total desc;
 
-# Mostrar el nombre, apellido y salario de la persona mejor pagada de un departamento concreto
+-- Mostrar el nombre, apellido y salario de la persona mejor pagada de un departamento concreto
 select first_name, last_name, salary from employees
     inner join dept_emp on employees.emp_no = dept_emp.emp_no
     inner join departments on dept_emp.dept_no = departments.dept_no and dept_emp.emp_no = employees.emp_no
@@ -10,7 +10,10 @@ where dept_name = 'Development'
 order by salary desc
 limit 1;
 
-# Mostrar el nombre, apellido y salario de la segunda mejor pagada de un departamento concreo
+
+
+
+-- Mostrar el nombre, apellido y salario de la segunda mejor pagada de un departamento concreo
 select first_name, last_name, salary from employees
     inner join dept_emp on employees.emp_no = dept_emp.emp_no
     inner join departments on dept_emp.dept_no = departments.dept_no and dept_emp.emp_no = employees.emp_no
@@ -18,10 +21,9 @@ select first_name, last_name, salary from employees
 where dept_name = 'Development'
 order by salary desc
 limit 1,1;
-# El limit 1,1 => salta el primer registro con el primer uno y con el segundo muestra el segundo registro.
+
+-- El limit 1,1 => salta el primer registro con el primer uno y con el segundo muestra el segundo registro.
 
 
-select * from salaries;
-
-
-select * from departments
+-- Mostrar el número de empleados contratados en un mes concreto
+select count(*) as numHireEmployees from employees where month(employees.hire_date) = 10;
