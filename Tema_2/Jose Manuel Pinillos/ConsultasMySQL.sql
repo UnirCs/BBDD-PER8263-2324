2acd3d023eb21d61be1c819afc073411da1f1c60
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2023-10-30 18:52:49 +0100

    Update ConsultasMySQL.sql

--- Tema_2/Jose Manuel Pinillos/ConsultasMySQL.sql
+++ Tema_2/Jose Manuel Pinillos/ConsultasMySQL.sql
@@ -1,12 +1,15 @@
 /* 1. Obtener el número de hombres y mujeres de la base de datos. Ordenar de forma descendente. */
-SELECT gender, count(*)
+SELECT gender as Genero, count(*)
 FROM employees
 GROUP BY gender
 
 /* 2. Mostrar el nombre, apellido y salario de la persona mejor pagada de un departamento concreto (parámetro variable). */
 SELECT first_name as Nombre, last_name as Apellido, salary as Salario
-FROM employees, salaries
-WHERE employees.emp_no = salaries.emp_no
+FROM employees, salaries, departments, dept_emp
+WHERE employees.emp_no = salaries.emp_no and
+      employees.emp_no = dept_emp.emp_no and
+      departments.dept_no = dept_emp.dept_no and
+      dept_emp.dept_no = 'd005'
 ORDER BY salary DESC
 LIMIT 1
 
