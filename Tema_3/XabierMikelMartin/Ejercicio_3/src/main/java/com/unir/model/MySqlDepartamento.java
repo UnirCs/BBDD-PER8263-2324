6497b6165fbf007ffba0b26b243f30388cf0bab1
XabierMikel <115424587+XabierMikel@users.noreply.github.com>
2023-11-01 14:34:54 +0100

    Entrega del ejercicio 3
    No entiendo qué hay que entregar en los documentos de inserción *.sql, todas las insert y updates se han hecho a través del proyecto java. He subido una carpeta que contiene las selects para chequear los resultados que se han introducido mediante el proyecto Java.

---
+++ Tema_3/XabierMikelMartin/Ejercicio_3/src/main/java/com/unir/model/MySqlDepartamento.java
@@ -0,0 +1,12 @@
+package com.unir.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MySqlDepartamento {
+    private String deptNo;
+    private String deptName;
+
+}
