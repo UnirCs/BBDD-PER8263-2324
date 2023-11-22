7eb453a3146971b0142d53e63763f851535f11dc
Yago Nieto Garrido <nietogarridoyago@gmail.com>
2023-11-22 10:49:51 +0100

    eliminacion proyectos

--- Tema_3/YagoNietoGarrido/InsercionesDepartments.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO departments (dept_no, dept_name) VALUES (?, ?);
-UPDATE departments SET dept_name = ? WHERE dept_no = ?;
--- Tema_3/YagoNietoGarrido/InsercionesEmployees.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO employees (emp_no, first_name, last_name, gender, hire_date, birth_date) VALUES (?, ?, ?, ?, ?, ?);
-UPDATE employees SET first_name = ?, last_name = ?, gender = ?, hire_date = ?, birth_date = ? WHERE emp_no = ?
