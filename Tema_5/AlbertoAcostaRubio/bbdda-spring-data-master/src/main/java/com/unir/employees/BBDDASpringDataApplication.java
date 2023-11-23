2a2e83f7bbff9c51b61e5b835f9cd069d34215b1
Alberto Acosta Rubio <143430005+alberotay@users.noreply.github.com>
2023-11-23 11:32:23 +0100

    Problemas de  concepto T4 createNativeQuery (#58)
    * Se entrega ejercicios de MySQL, no consigo cargar drivers Oracle de momento
    
    * Cambio de IDE a IntelliJ
    
    * Delete .vscode directory
    
    * Problemas de concepto T4
    
    * Update ejercicio Tema 5

---
+++ Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/src/main/java/com/unir/employees/BBDDASpringDataApplication.java
@@ -0,0 +1,17 @@
+package com.unir.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBDDASpringDataApplication {
+
+	/**
+	 * Método principal de la aplicación.
+	 * @param args - argumentos de la aplicación.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(BBDDASpringDataApplication.class, args);
+	}
+
+}
