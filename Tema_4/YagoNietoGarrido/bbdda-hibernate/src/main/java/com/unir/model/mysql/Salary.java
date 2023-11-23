7bf1f8d13ec7b4cc1cb1f1d14761bf6edd9c5beb
YagoUnir <148803053+YagoUnir@users.noreply.github.com>
2023-11-23 11:24:38 +0100

    Ejercicio2, 3 y 4 Yago Nieto Garrido (#54)
    * Ejercicio2 Yago Nieto
    
    * Ejercicio2 Yago Nieto
    
    * Ejercicio 3 Yago Nieto Garrido
    
    * Ejercicio 4
    
    * eliminacion proyectos
    
    * Ejercicio 3 y 4 puestos
    
    ---------
    
    Co-authored-by: Yago Nieto Garrido <nietogarridoyago@gmail.com>

---
+++ Tema_4/YagoNietoGarrido/bbdda-hibernate/src/main/java/com/unir/model/mysql/Salary.java
@@ -0,0 +1,35 @@
+package com.unir.model.mysql;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "salaries")
+@IdClass(SalaryId.class)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Salary {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "emp_no")
+    private Employee empNo;
+
+    @Id
+    @Column(name = "from_date", columnDefinition="DATE")
+    private Date fromDate;
+
+    @Column(name = "to_date", columnDefinition="DATE")
+    private Date toDate;
+
+    @Column(name = "salary")
+    private int salary;
+
+}
