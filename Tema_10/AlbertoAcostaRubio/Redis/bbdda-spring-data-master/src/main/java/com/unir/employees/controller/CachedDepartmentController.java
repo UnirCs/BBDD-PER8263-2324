290c80c396b07584b5e1570acdad22988a67cb48
Alberto Acosta Rubio <143430005+alberotay@users.noreply.github.com>
2024-01-12 09:22:26 +0100

    Entrega actividad DynamoDB y Redis (#87)
    * Se entrega ejercicios de MySQL, no consigo cargar drivers Oracle de momento
    
    * Cambio de IDE a IntelliJ
    
    * Delete .vscode directory
    
    * Problemas de concepto T4
    
    * Update ejercicio Tema 5
    
    * Entrega actividad Elasticsearch
    
    * Entrega actividad DynamoDB
    
    * Problema conexión con Redis
    
    * Entrega Redis y DynamoDB
    
    * Delete Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/src/main/resources/application.yml
    
    * Delete Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/src/main/java/com/unir/employees/model/db/Department.java
    
    * Delete Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/DepartmentController.java
    
    * Delete Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/CachedDepartmentController.java
    
    * Delete Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/src/main/java/com/unir/employees/BBDDASpringDataApplication.java
    
    * Delete Tema_5/AlbertoAcostaRubio/bbdda-spring-data-master/pom.xml
    
    * Delete Tema_10/AlbertoAcostaRubio/Redis/bbdda-spring-data-master/src/main/java/com/unir/employees/model/db/Department.java

--- Tema_10/AlbertoAcostaRubio/Redis/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/CachedDepartmentController.java
+++ Tema_10/AlbertoAcostaRubio/Redis/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/CachedDepartmentController.java
@@ -1,15 +1,18 @@
 package com.unir.employees.controller;
-import java.util.List;
+
 import com.unir.employees.data.DepartmentRepository;
 import com.unir.employees.model.db.Department;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/departments")
 @RequiredArgsConstructor
-public class DepartmentController {
+public class CachedDepartmentController {
 
     private final DepartmentRepository departmentRepository;
 
@@ -19,9 +22,10 @@ public class DepartmentController {
      * @param deptName - nombre del departamento.
      * @return departamento.
      */
-    @GetMapping("/{name}")
-    public ResponseEntity<Department> getDepartmentByName(@PathVariable("name") String deptName) {
-        return ResponseEntity.ok(departmentRepository.findByDeptName(deptName).orElse(null));
+    @GetMapping("/api-cached/departments/{name}")
+    @Cacheable(value = "department", key = "#deptName")
+    public Department getDepartmentByName(@PathVariable("name") String deptName) {
+        return departmentRepository.findByDeptName(deptName).orElse(null);
     }
 
     /**
@@ -30,12 +34,5 @@ public class DepartmentController {
      * @param department - departamento.
      * @return departamento creado.
      */
-    @PostMapping("/")
-    public Department createDepartment(@RequestBody Department department) {
-        return departmentRepository.save(department);
-    }
-    @GetMapping("/departamentos")
-    public List<Department> findAllByOrderByDeptNameAsc() {
-        return departmentRepository.findAllByOrderByDeptNameAsc();
-    }
+
 }
