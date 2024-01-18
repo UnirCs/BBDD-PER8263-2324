9b2f43f548f074ed991e08cfcf3419808629c20d
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2024-01-18 20:33:48 +0100

    aplicacion de cambios

---
+++ Tema_10/Jose Manuel Pinillos/Redis/Jose Manuel Pinillos/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/CachedDepartmentController.java
@@ -0,0 +1,23 @@
+package com.unir.employees.controller;
+
+import com.unir.employees.data.DepartmentRepository;
+import com.unir.employees.model.db.Department;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+public class CachedDepartmentController {
+
+    private final DepartmentRepository departmentRepository;
+
+    @GetMapping("/api-cached/departments/{name}")
+    @Cacheable(value = "department", key = "#deptName")
+    public Department getDepartmentByName(@PathVariable("name") String deptName) {
+        return departmentRepository.findByDeptName(deptName).orElse(null);
+    }
+}
