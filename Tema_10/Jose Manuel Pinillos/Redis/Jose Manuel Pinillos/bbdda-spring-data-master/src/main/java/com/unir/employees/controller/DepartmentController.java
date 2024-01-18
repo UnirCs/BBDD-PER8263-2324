f678ffa74005f1c4cc851bc9478bb8d6fabe4d71
Jose Manuel Pinillos Rubio <pinillosrubio@gmail.com>
2024-01-18 20:40:43 +0100

    update

--- Tema_10/Jose Manuel Pinillos/Redis/Jose Manuel Pinillos/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/DepartmentController.java
+++ Tema_10/Jose Manuel Pinillos/Redis/Jose Manuel Pinillos/bbdda-spring-data-master/src/main/java/com/unir/employees/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package com.unir.employees.controller;
 import com.unir.employees.data.DepartmentRepository;
 import com.unir.employees.model.db.Department;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class DepartmentController {
 
     private final DepartmentRepository departmentRepository;
 
-    /*
+/*
     @GetMapping("/{name}")
     public ResponseEntity<Department> getDepartmentByName(@PathVariable("name") String deptName) {
         return ResponseEntity.ok(departmentRepository.findByDeptName(deptName).orElse(null));
