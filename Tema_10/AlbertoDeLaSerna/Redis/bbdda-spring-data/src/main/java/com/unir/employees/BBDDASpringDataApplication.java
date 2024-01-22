7f2a4cac5490a5d519a1485d994399ed6f73a14f
adelasernaunir <148379696+adelasernaunir@users.noreply.github.com>
2024-01-22 14:35:27 +0100

    Entrego el ejercicio pero da error `cacheManager` (#92)
    * Primeras queries
    
    * oracle queries iniciales
    
    * Las consultas de oracle terminadas
    
    * He metido el objeto XMLForest en la segunda query de Oracle
    
    * He actualizado también el program de Java, he cambiado la segunda consulta de Oracle. He incluido el objeto XMLForest
    
    * Había machacado el fichero sql de Oracle ;-)
    
    * Entrega actividad 3
    
    * Actividad del Tema 4 acabada
    
    * Actividad 5 finalizada
    
    * Actividad 7 acabada
    
    * He quitado la propiedad dept_no de la entidad salary
    
    * Me he quedado en la parte V, mañan más y mejor ;-)
    
    * Ejercicio elasticsearch acabado
    
    * Nuevos merges
    
    * Redis con error
    
    * He corregido el ejercicio de Redis, comentado en el readme y también incluyo el resultado de redis
    
    * Actividad Neo4j completada
    
    * Cambiado un poco el readme de Neo4j

---
+++ Tema_10/AlbertoDeLaSerna/Redis/bbdda-spring-data/src/main/java/com/unir/employees/BBDDASpringDataApplication.java
@@ -0,0 +1,20 @@
+package com.unir.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+
+@SpringBootApplication
+@EnableCaching
+public class BBDDASpringDataApplication {
+
+	/**
+	 * Método principal de la aplicación.
+	 * @param args - argumentos de la aplicación.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(BBDDASpringDataApplication.class, args);
+	}
+
+}
